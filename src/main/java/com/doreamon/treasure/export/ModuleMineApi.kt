e500a47e3106074323b90c1bb2569d5cc57fbd0f
zhanhua <913447093@qq.com>
2023-05-25 16:55:15 +0800

    first commit

--- src/main/java/com/doreamon/treasure/export/ModuleMineApi.kt
+++ src/main/java/com/doreamon/treasure/export/ModuleMineApi.kt
@@ -1,17 +1,15 @@
 package com.doreamon.treasure.export
 
-import androidx.fragment.app.Fragment
-import com.alibaba.android.arouter.launcher.ARouter
 
 /**
  * @author wzh
  * @date 2023/2/9
  */
-object ModuleMineApi {
+//object ModuleMineApi {
 
-    const val ROUTER_MINE_MINE_FRAGMENT = "/mine/MineFragment"
-
-    fun getMineFragment(): Fragment {
-        return ARouter.getInstance().build(ROUTER_MINE_MINE_FRAGMENT).navigation() as Fragment
-    }
-}
+//    const val ROUTER_MINE_MINE_FRAGMENT = "/mine/MineFragment"
+//
+//    fun getMineFragment(): Fragment {
+//        return ARouter.getInstance().build(ROUTER_MINE_MINE_FRAGMENT).navigation() as Fragment
+//    }
+//}
--- src/main/java/com/doreamon/treasure/utils/Resource.kt
+++ src/main/java/com/doreamon/treasure/utils/Resource.kt
@@ -1,7 +1,10 @@
 package com.doreamon.treasure.utils
 
 import android.content.Context
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import com.doreamon.treasure.base.App
 
 /**
@@ -14,4 +17,15 @@ import com.doreamon.treasure.base.App
 @JvmOverloads
 fun getStringById(@StringRes resId: Int, context: Context = App.instance): String {
     return context.getString(resId)
+}
+
+
+/**
+ * 根据资源id[colorResId] 获取颜色值[Int]
+ * > [context] 可选，默认[AppManager.getContext]
+ */
+@ColorInt
+@JvmOverloads
+fun getColorById(@ColorRes colorResId: Int, context: Context = AppManager.getContext()): Int {
+    return ContextCompat.getColor(context, colorResId)
 }
