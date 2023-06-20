b17063e683644606f9a9c9aa9df32dddd363ec11
zhanhua <913447093@qq.com>
2023-06-20 17:33:07 +0800

    接入首页接口

--- src/main/java/com/doreamon/treasure/utils/ImageUtil.kt
+++ src/main/java/com/doreamon/treasure/utils/ImageUtil.kt
@@ -1,9 +1,16 @@
 package com.doreamon.treasure.utils
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
 /**
  * @author wzh
  * @date 2022/5/11
  */
-class ImageUtil {
+object ImageUtil {
+
+    fun loadUrl(iv: ImageView, url: String?) {
+        Glide.with(iv.context).load(url).into(iv)
+    }
 
 }
